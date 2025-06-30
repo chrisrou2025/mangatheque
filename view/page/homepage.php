e50ae259fab3ca580d0fac30f07f9dbd6ed26b45
chrisrou2025 <ch.roupioz@laposte.net>
2025-06-30 14:17:57 +0200

    base html template

--- view/page/homepage.php
+++ view/page/homepage.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello World !</h1>
-</body>
-</html>
+<?php
+$title = "Page d'accueil";
+// $content = "coucou";
+// Include the base HTML template
+require_once 'view/base-html.php';
