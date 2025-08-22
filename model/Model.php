8a0e16a2da1807fa5dc143ab95703e18d999fa63
Christian ROUPIOZ <ch.roupioz@laposte.net>
2025-08-22 11:38:55 +0200

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -10,12 +10,14 @@ abstract class Model
     private static function setDb()
     {
         try {
-            // Assurez-vous que les informations de connexion sont correctes pour votre environnement
+            // Connexion à la base de données avec PDO
             self::$db = new PDO('mysql:host=localhost;dbname=mangatheque', 'root', 'root');
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Ajoute la gestion des erreurs
         } catch (PDOException $e) {
+            // En cas d'erreur de connexion, on affiche un message et on arrête l'exécution
             echo "Erreur de connexion à la base de données : " . $e->getMessage();
-            // En production, vous devriez logger l'erreur au lieu de l'afficher directement
+            // Optionnel : log l'erreur pour le débogage
+            error_log($e->getMessage()); // Log l'erreur
             exit(); // Arrête l'exécution si la connexion échoue
         }
     }
